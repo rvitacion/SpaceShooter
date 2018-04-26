667b9d10fc7fc26f700c9c2cc583596e6773d952
Gerardo Rodriguez <Gerardo.Rodriguez.844@my.csun.edu>
2018-04-26 15:39:49 -0700

    Camera under player, no more flycamera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 7;
+    private Rigidbody rb;
+
+    #region MonoBehaviour API
+
+    private void start()
+    {
+        rb = GetComponent <Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 300.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        rb.AddForce(movement * speed);
+    }
+
+    #endregion
+}
