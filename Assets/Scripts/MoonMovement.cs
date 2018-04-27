1c1121ed757a54d97b31df451bc6cf841be8a1c5
Gerardo Rodriguez <Gerardo.Rodriguez.844@my.csun.edu>
2018-04-26 19:54:24 -0700

    CamerLook fixed, added astreroids and orbit

---
+++ Assets/Scripts/MoonMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonMovement : MonoBehaviour {
+
+    public GameObject earth;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(earth.transform.position, Vector3.up, 70 * Time.deltaTime);
+    }
+}
